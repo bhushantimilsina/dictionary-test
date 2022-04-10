6c581d662061b6271fd5a0520e41fb77342c6518
bhushan <bhushantimilsina@gmail.com>
2022-04-10 09:35:45 -0400

    Improved the per even more

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-java -Ddict.filepath=/mnt/c/dos/dictionary/english3.txt -cp /mnt/c/dev/repo/dictionary-test/out/production/dictionary-test AlphaCountApp
+cd /mnt/c/dev/repo/dictionary-test/out/production/dictionary-test/
+java -Ddict.filepath=/mnt/c/dev/repo/dictionary-test/data/english5.txt CountAlphaMapAsync $1
+cd -
